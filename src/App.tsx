581c37d8c3c655ada97078c6b03b7aa81f0c26c3
king120kw <king120kw@users.noreply.github.com>
2026-07-18 08:22:03 +0700

    Fix Navigate import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import Welcome from "./pages/Welcome";
